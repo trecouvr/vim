ab97e26bfc3d672c51a265dc8d90f0cf7bb617af
Thomas Recouvreux <thomas.recouvreux@polyconseil.fr>
2014-04-28 13:28:40 +0200

    Simplify setup

---
+++ README.md
@@ -0,0 +1,8 @@
+
+setup
+=====
+
+    git clone <...> .vim
+    ln -s .vim/vimrc .vimrc
+    cd .vim
+    ./git_update.sh
---
+++ git_update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -x
+
+git submodule init
+git submodule update
+git submodule foreach git submodule init
+git submodule foreach git submodule update
